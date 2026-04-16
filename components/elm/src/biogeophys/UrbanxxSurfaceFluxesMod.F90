dfc9ffc37a20c739b48c62818a18d7b7d7724c0d
Gautam Bisht <gautam.bisht@pnnl.gov>
2026-04-16 16:55:20 -0700

    URBANxx: remove SetFwetValues; fwet now computed internally in URBANxx
    ELM no longer passes h2osoi_liq/ice or snow_depth for the surfaceFluxes
    step.  URBANxx computes fwet on-the-fly from its own persistent
    TopH2OSoiLiq / TopH2OSoiIce views using the same exponent (0.666666666666)
    as ELM's UrbanFluxesMod.F90.
    
    Changes:
    - UrbanxxSurfaceFluxesMod.F90: remove fwet_road/fwet_roof module buffers,
      their allocations, use ColumnDataType import, the call to SetFwetValues,
      and the entire SetFwetValues subroutine (including use of col_ws,
      pondmx_urban, UrbanSetFractionWetRoof/ImperviousRoad)
    - ELM_to_UrbanSet_Mapping.md: clear surfaceFluxes checkmarks for
      h2osoi_liq and h2osoi_ice; mark snow_depth as no longer sent;
      update Notes column for all three rows

--- components/elm/src/biogeophys/UrbanxxSurfaceFluxesMod.F90
+++ components/elm/src/biogeophys/UrbanxxSurfaceFluxesMod.F90
@@ -13,7 +13,6 @@ module UrbanxxSurfaceFluxesMod
   use UrbanParamsType      , only : urbanparams_type
   use SurfaceAlbedoType    , only : surfalb_type
   use FrictionVelocityType , only : frictionvel_type
-  use ColumnDataType       , only : col_ws
   use UrbanxxInstanceMod   , only : urbanxx
   use UrbanxxMod           , only : SetHeightParameters
   use VegetationDataType   , only : veg_ef, veg_wf
@@ -23,10 +22,6 @@ module UrbanxxSurfaceFluxesMod
   implicit none
 
   private
-
-  ! Persistent input buffers (allocated once in init)
-  real(c_double) , allocatable, target :: fwet_road(:)
-  real(c_double) , allocatable, target :: fwet_roof(:)
 
   ! Persistent output buffers (allocated once in init)
   real(c_double) , allocatable, target, public :: eflx_sh_grnd_roof(:)
@@ -64,10 +59,6 @@ contains
     !
     implicit none
     integer(c_int), intent(in) :: num_urbanl
-
-    ! Input buffers
-    allocate(fwet_road(num_urbanl))
-    allocate(fwet_roof(num_urbanl))
 
     ! Output buffers
     allocate(eflx_sh_grnd_roof(num_urbanl))
@@ -118,8 +109,6 @@ contains
     real(r8)                             :: err_eflx_sh_grnd, err_qflx_evap_soi, err_cgrnds, err_cgrndl
     real(r8)                             :: rel_eflx_sh_grnd, rel_qflx_evap_soi, rel_cgrnds, rel_cgrndl
     integer :: idx_roof, idx_road_improv, idx_road_perv, idx_sunwall, idx_shadwall, idx_landunit
-
-    call SetFwetValues(urbanxx, num_urbanl, num_urbanc, filter_urbanc)
 
     call UrbanComputeSurfaceFluxes(urbanxx, status)
     if (status /= URBAN_SUCCESS) call UrbanError(iam, __LINE__, status)
@@ -294,73 +283,5 @@ contains
       call exit(0)
    end if
   end subroutine urbanxx_surfaceFluxes
-
-  !-----------------------------------------------------------------------
-  subroutine SetFwetValues(urban, num_urbanl, num_urbanc, filter_urbanc)
-    !
-    ! Set fraction wet values for impervious road based on snow depth and ponding
-    !
-    use WaterStateType, only : waterstate_type
-    use elm_varcon, only : pondmx_urban
-    use ColumnType, only : col_pp
-    use column_varcon, only : icol_roof, icol_road_imperv
-    !
-    implicit none
-    !
-    type(UrbanType)      , intent(in) :: urban
-    integer(c_int)       , intent(in) :: num_urbanl
-    integer(c_int)       , intent(in) :: num_urbanc
-    integer              , intent(in) :: filter_urbanc(:) ! urban column filter
-    !
-    integer(c_int)                       :: status
-    integer                              :: fc, l, c, idx_road, idx_roof
-    real(r8)                             :: fwet
-
-    associate(                             &
-         snow_depth => col_ws%snow_depth , & ! Input: [real(r8) (:)] snow depth (m)
-         h2osoi_liq => col_ws%h2osoi_liq , & ! Input: [real(r8) (:,:)] liquid water (kg/m2)
-         h2osoi_ice => col_ws%h2osoi_ice   & ! Input: [real(r8) (:,:)] ice water (kg/m2
-         )
-
-      ! Loop through urban landunits
-      idx_road = 0
-      idx_roof = 0
-      do fc = 1, num_urbanc
-        c = filter_urbanc(fc)
-
-        if (col_pp%itype(c) == icol_roof) then
-           idx_roof = idx_roof + 1
-           if (snow_depth(c) > 0._r8) then
-              fwet = min(snow_depth(c)/0.05_r8, 1._r8)
-           else
-              fwet = (max(0._r8, h2osoi_liq(c,1)+h2osoi_ice(c,1))/pondmx_urban)**0.666666666666_r8
-              fwet = min(fwet,1._r8)
-           end if
-           fwet_roof(idx_roof) = fwet
-        end if
-        if (col_pp%itype(c) == icol_road_imperv) then
-           idx_road = idx_road + 1
-           ! Calculate fraction wet based on snow depth or ponding
-           ! From UrbanFluxesMod.F90:L577-582
-           if (snow_depth(c) > 0._r8) then
-              fwet = min(snow_depth(c)/0.05_r8, 1._r8)
-           else
-              fwet = (max(0._r8, h2osoi_liq(c,1)+h2osoi_ice(c,1))/pondmx_urban)**0.666666666666_r8
-              fwet = min(fwet, 1._r8)
-          end if
-          fwet_road(idx_road) = fwet
-        endif
-      end do
-
-      ! Set the values in urbanxx
-      call UrbanSetFractionWetImperviousRoad(urban, c_loc(fwet_road), num_urbanl, status)
-      if (status /= URBAN_SUCCESS) call UrbanError(iam, __LINE__, status)
-
-      call UrbanSetFractionWetRoof(urban, c_loc(fwet_roof), num_urbanl, status)
-      if (status /= URBAN_SUCCESS) call UrbanError(iam, __LINE__, status)
-
-    end associate
-
-  end subroutine SetFwetValues
 
 end module UrbanxxSurfaceFluxesMod
